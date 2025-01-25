7e4b27747a680da6d0bf6222f74d4bf1d09f1ea7
abneerodriggo <abneerodriggo@gmail.com>
2025-01-25 08:55:54 -0300

    Seed Game

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -13,22 +13,26 @@ public class Game {
     @Column (name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
-    private String shorDescription;
+    @Column (columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column (columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-        public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shorDescription, String longDescription) {
+        public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
             this.id = id;
             this.title = title;
             this.year = year;
             this.genre = genre;
-            this.platform = platform;
+            this.platforms = platforms;
+            this.score = score;
             this.imgUrl = imgUrl;
-            this.shorDescription = shorDescription;
+            this.shortDescription = shortDescription;
             this.longDescription = longDescription;
         }
 
@@ -64,15 +68,23 @@ public class Game {
             this.genre = genre;
         }
 
-        public String getPlatform() {
-            return platform;
+        public String getPlatforms() {
+            return platforms;
         }
 
-        public void setPlatform(String platform) {
-            this.platform = platform;
+        public void setPlatform(String platforms) {
+            this.platforms = platforms;
         }
 
-        public String getImgUrl() {
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
             return imgUrl;
         }
 
@@ -80,12 +92,12 @@ public class Game {
             this.imgUrl = imgUrl;
         }
 
-        public String getShorDescription() {
-            return shorDescription;
+        public String getShortDescription() {
+            return shortDescription;
         }
 
-        public void setShorDescription(String shorDescription) {
-            this.shorDescription = shorDescription;
+        public void setShortDescription(String shortDescription) {
+            this.shortDescription = shortDescription;
         }
 
         public String getLongDescription() {
